1b22dabe1d613aff07b5eaa3826fdc5c20664850
Vital-pro <vitupikov@yandex.ru>
2024-06-30 10:22:31 +0300

    upgrade

--- server-ii.js
+++ server-ii.js
@@ -1,19 +1,14 @@
+require('dotenv').config();
+const express = require('express');
 const cheerio = require('cheerio');
-const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
 
-const fs = require('fs')
+const app = express();
+const PORT = process.env.PORT || 3333;
 
-// const data1 = fs.readFileSync('./posts.json', 'utf8'); //!
-// const data2 = JSON.parse(data1); //!
-// const articles_data = JSON.parse(data);
-// console.log(data2);
-const app = express()
-const PORT = process.env.PORT || 3333
+//***start***CRON***** */
 
-//***start******** */
-
-const interval = 60000;
+const interval = 10000;
 function cron(ms, fn) {
   async function cb() {
     clearTimeout(timeout);
@@ -22,73 +17,84 @@ function cron(ms, fn) {
   }
   let timeout = setTimeout(cb, ms);
   // return () => clearTimeout(timeout);
-  return () => {};
+  return () => { };
 }
 
 //***finish*********** */
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
-const url = 'http://vesti-sudak.ru/'
+// const url = 'http://vesti-sudak.ru/';
+const url = '`https://the-internet.herokuapp.com/dynamic_content`';
 
+// const articles_data1 = [];
 
-const articles_data1 = []
+// async function getGanre() {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.text();
+//     const $ = cheerio.load(data);
 
-async function getGanre() {
-  try {
-    const response = await fetch(url)
-    const data = await response.text()
-    const $ = cheerio.load(data)
+//     const articles = $('article');
+//     articles.each(function () {
+//       // link = $(this).find('h1').text() // test
+//       link = $(this).find('h2 > a').attr('href'); // official
+//       title = $(this).find('h2 > a').text(); // official
+//       // image = $(this).find('div > a > img').attr('srcset').split(',') // official
+//       image = $(this).find('div > a > img').attr('srcset').split(' ')[0].trim(); // official
 
-    const articles = $('article')
-    articles.each(function() {
-      // link = $(this).find('h1').text() // test
-      link = $(this).find('h2 > a').attr('href') // official
-      title = $(this).find('h2 > a').text() // official
-      // image = $(this).find('div > a > img').attr('srcset').split(',') // official
-      image = $(this).find('div > a > img').attr('srcset').split(' ')[0].trim() // official
+//       // articles_data.push({link, title, image})  // official
+//       articles_data1.splice(9);
+//       articles_data1.push({ link, title, image }); // official
+//       // articles_data.push({link}) // test
+//     });
 
-      // articles_data.push({link, title, image})  // official
-      articles_data1.splice(9)
-      articles_data1.push({link, title, image})  // official
-      // articles_data.push({link}) // test
-    })
+//     // console.log(articles_data);
 
-    // fs.writeFileSync('./posts.json', JSON.stringify(articles_data1)); //!
+//     return articles_data1; //todo
 
-
-    // console.log(articles_data);
-
-    return articles_data1 //todo
-
-    // console.log(articles_data);
-    // console.log(articles_data.length);
-
-
-  } catch (error) {
-    console.error(error)
-    console.log(error.message);
-  }
-}
+//     // console.log(articles_data);
+//     // console.log(articles_data.length);
+//   } catch (error) {
+//     console.error(error);
+//     console.log(error.message);
+//   }
+// }
 
 // getGanre()
+
+async function getNews() {
+  let news = [];
+  const response = await fetch(
+    `https://the-internet.herokuapp.com/dynamic_content`
+  );
+  const html = await response.text();
+  const $ = cheerio.load(html);
+
+  $('#content').each((tdx, el) => {
+    let image =
+      `https://the-internet.herokuapp.com` +
+      $(el).find('.row > div > img').attr('src');
+    news.push({ image });
+  });
+  app.locals.news = news;
+  console.log(app.locals.news);
+  return app.locals.news;
+}
+
+
 app.get('/start', (req, res) => {
-  cron(interval, () => getGanre().then((f) => console.log(f.length)));              //!!!!!! ******START!!!
-  res.redirect('/')
-  res.status(200).end()
-})
+  getNews();
+  cron(interval, () => getNews()); //! раскомменть и запустится crone
+  // res.redirect('/');
+  res.status(200).json(app.locals.news);
+});
 
 // routes
 app.get('/news', (req, res) => {
-  // const {subreddit} = req.params
-  // console.log(news);
-  // const data = getGanre()
-    res.status(200).json(articles_data1); 
+  res.status(200).json(app.locals.news);
+});
 
-  
-})
-
-
-app.listen(PORT, () => console.log('Server started!.. on port:', PORT))
+app.listen(PORT, () => console.log('Server started!.. on port:', PORT));
